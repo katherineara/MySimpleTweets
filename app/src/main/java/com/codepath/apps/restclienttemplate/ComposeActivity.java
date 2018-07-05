8ddd1892b9a8716f92aae1f8e736829cb98af714
Katherine Arackaparambil <katherineara@fb.com>
2018-07-05 15:03:46 -0700

    Character count

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,9 +3,12 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -21,7 +24,7 @@ public class ComposeActivity extends AppCompatActivity {
     TwitterClient client;
     EditText message;
     JsonHttpResponseHandler handler;
-    Tweet tweet;
+    TextView counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +34,12 @@ public class ComposeActivity extends AppCompatActivity {
 
         Button button = (Button) findViewById(R.id.tweet_button);
         message = (EditText) findViewById(R.id.tweet_message);
+        counter = (TextView) findViewById(R.id.counter);
         handler = new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
             Tweet tweet = null;
-            try {
+                try {
                     tweet = Tweet.fromJSON(response);
                     Intent intent = new Intent(ComposeActivity.this, TimelineActivity.class);
                     intent.putExtra(Tweet.class.getSimpleName(), Parcels.wrap(tweet));
@@ -51,5 +55,21 @@ public class ComposeActivity extends AppCompatActivity {
                 client.sendTweet(message.getText().toString(), handler);
             }
         });
+
+        final TextWatcher textEditor = new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                counter.setText(String.valueOf(140 - s.length()));
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        message.addTextChangedListener(textEditor);
     }
+
+
 }
